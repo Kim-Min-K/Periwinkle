05abda62cb7e5cd771d57810f031de60263d4bd5
setsooner <kairoaran@gmail.com>
2025-02-08 22:17:34 +0000

    redirect base url to login

--- periwinkleposts/periwinkleposts/urls.py
+++ periwinkleposts/periwinkleposts/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('inbox/', include('inbox.urls')),
     path('pages/', include('pages.urls')),
     path('accounts/', include('accounts.urls')),
+    path('', RedirectView.as_view(url='/accounts/login/', permanent=False)),
 ]
