a8c3545c48f57975c50394e5d69d82d052c20df8
abinandn1 <abinand.nanthan@gmail.com>
2025-02-18 22:04:08 -0700

    added CommonMark to allow for markdown in post content

--- periwinkleposts/pages/views.py
+++ periwinkleposts/pages/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from accounts.models import Post
+import commonmark
+from django.utils.safestring import mark_safe
 
 
 def homeView(request):
     user = request.user
     posts = Post.objects.all().order_by("-published")
+
+    #convert content in posts where contentType == "text/markdown"
+    for post in posts:
+        if post.contentType == "text/markdown":
+            post.content = markdown_to_html(post.content)
 
     context = {
         "username": user.username,
@@ -13,3 +20,9 @@ def homeView(request):
     }
 
     return render(request, "home.html", context)
+
+def markdown_to_html(md_text):
+    #use CommonMark functions to convert markdown to html
+    parser = commonmark.Parser()
+    renderer = commonmark.HtmlRenderer()
+    return mark_safe(renderer.render(parser.parse(md_text)))
