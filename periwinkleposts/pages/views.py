312a98bf3667641c95835e1ec48f95ccd8b93801
Kim-Min-K <mkkim1@ualberta.ca>
2025-02-18 19:05:04 -0700

    Can now view posts on home.html

--- periwinkleposts/pages/views.py
+++ periwinkleposts/pages/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from accounts.models import Post
+
 
 def homeView(request):
     user = request.user
+    posts = Post.objects.all().order_by("-published")
+
     context = {
         "username": user.username,
         "github_username": user.github_username,
+        "posts": posts,
     }
+
     return render(request, "home.html", context)
