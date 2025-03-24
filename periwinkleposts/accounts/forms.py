a237e65db3b46f0e156c84dc120d1ea4dd7ed0eb
ryan <rom@ualberta.ca>
2025-03-23 22:33:37 -0600

    add DisplayName sectio in register

--- periwinkleposts/accounts/forms.py
+++ periwinkleposts/accounts/forms.py
@@ -5,9 +5,11 @@ from .models import Authors
 class AuthorCreation(UserCreationForm):
     github_username = forms.CharField(max_length=100, required=False)
     host = forms.CharField(max_length=200, required=True)
+    displayName = forms.CharField(max_length=200, required=True)
+    
     class Meta:
         model = Authors
-        fields = ("username", "host", "github_username", "password1", "password2")
+        fields = ("username", "displayName", "host", "github_username", "password1", "password2")
         
 # simple form that defines what model to alter and what field
 class AvatarUpload(forms.ModelForm):
