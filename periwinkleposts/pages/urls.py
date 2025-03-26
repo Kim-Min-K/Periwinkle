ac035cf169d4fd1f6d34780f8863e7b241cbf7ce
Kim-K-Min <mkkim1@ualberta.ca>
2025-03-26 08:17:37 +0000

    Revert "Basic inbox UI implementation"
    This reverts commit b69430074b3a10657b0b8a5d79b23f478c1ccf64.

--- periwinkleposts/pages/urls.py
+++ periwinkleposts/pages/urls.py
@@ -5,7 +5,5 @@ app_name = 'pages'
 
 urlpatterns = [
     path('home/', views.homeView, name='home'),
-    path('nodereg/', views.nodeView, name='node'),
-    path('inbox/<uuid:row_id>', views.inboxView, name='inbox'),
-
+    path('nodereg/', views.nodeView, name='node')
 ]
