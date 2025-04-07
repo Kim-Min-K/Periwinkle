7c4b148745cca8d1824de96c2f5c9783d5e42b16
Kim-K-Min <mkkim1@ualberta.ca>
2025-04-07 21:27:21 +0000

    image breaking from debug fix

--- periwinkleposts/periwinkleposts/settings.py
+++ periwinkleposts/periwinkleposts/settings.py
@@ -30,7 +30,7 @@ STATICFILES_DIRS = [
 SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = False
+DEBUG = True
 # ALLOWED_HOSTS = ["*"]
 ALLOWED_HOSTS = ["*"]
 # Application definition
